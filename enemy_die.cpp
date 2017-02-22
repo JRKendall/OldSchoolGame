7bc573008e6ecf8c07e41c23635f0d68b75729cd
jordonc24 <cadorej@uni.coventry.ac.uk>
2017-02-22 12:15:20 +0000

    Update enemy_die.cpp

--- enemy_die.cpp
+++ enemy_die.cpp
@@ -12,12 +12,13 @@ int main()
 
 int die(self, map)
 	{
-	 map.removelist.emplace_back(map.enemyList.pop(self.ref)[1]; // Adds the second element of the nested enemy vector, and appends it to map.removeList. 
+	 map.removelist.emplace_back(map.enemyList.pop(self.ref))[1]; //  Ads the second element of the nested enemy vector, and appends it to map.removeList.
 	 for ( int i; i < map.enemyList.size; map.enemyList[i][2].ref = i ) // Bad solution. May be replaced later
 		{
-		 map.updateEnemies(0)
-		 self.drop_gold(self.golddropped) // Runs he function that causes enemies to drop gold
+		 
  		}
+	map.updateEnemies(0)
+	self.drop_gold(self.golddropped) // Runs the unction that causes enemies to drop gold
 
 	 return 0;
 	}
