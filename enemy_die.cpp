5389138e704edcbed59be72d566a793ccdc0f3de
jordonc24 <cadorej@uni.coventry.ac.uk>
2017-02-22 12:06:41 +0000

    Add files via upload

---
+++ enemy_die.cpp
@@ -0,0 +1,23 @@
+#include <array>
+#include <vector>
+#include <iostream>
+#include <queue>
+#include <functional>
+using namespace std;
+
+int main()
+	{
+	 return 0;
+	}
+
+int die(self, map)
+	{
+	 map.removelist.emplace_back(map.enemyList.pop(self.ref)[1]; // Adds the second element of the nested enemy vector, and appends it to map.removeList. 
+	 for ( int i; i < map.enemyList.size; map.enemyList[i][2].ref = i ) // Bad solution. May be replaced later
+		{
+		 map.updateEnemies(0)
+		 self.drop_gold(self.golddropped) // Runs he function that causes enemies to drop gold
+ 		}
+
+	 return 0;
+	}
