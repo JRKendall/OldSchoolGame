b06c80ccbcd09bb7032d7c600ffd54bf6de7f96e
JRKendall <joshkendall100@gmail.com>
2017-03-15 12:18:58 +0000

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -20,10 +20,11 @@ vector<vector<int>> enemyLocList;
 vector<int> enemyTypeList;
 vector<map<char, int>> enemyStatList;
 map<char, int> playerStatList;
+map<vector<int>, int> goldList;
 vector<vector<int>> world;
 
 map<char, int> setStats(int ref){
-	//Gets the stats of the player from the database to be used in the game
+	//Lookup stats in stat lists, for now defaults used
 	map<char, int> stats;
 	stats['H'] = 100; //HP
 	stats['A'] = 30; //Attack
@@ -38,9 +39,13 @@ vector<vector<int>> updatePlayer(int dx, int dy, vector<vector<int>> world = wor
 		return world;
 	}
 	world.at(playerLocation.at(0)).at(playerLocation.at(1)) = -1;
-	if(world.at(playerLocation.at(0)+dx).at(playerLocation.at(1)+dy) == -1 || initial){
+	if(world.at(playerLocation.at(0)+dx).at(playerLocation.at(1)+dy) == -1 ||
+	   world.at(playerLocation.at(0)+dx).at(playerLocation.at(1)+dy) == -5 || initial){
 		playerLocation.at(0) += dx;
 		playerLocation.at(1) += dy;
+		if(world.at(playerLocation.at(0)).at(playerLocation.at(1)) == -5){
+			playerStatList['G'] += goldList[{playerLocation.at(0), playerLocation.at(1)}];
+		}
 	}
 	world.at(playerLocation.at(0)).at(playerLocation.at(1)) = -2;
 	return world;
@@ -55,13 +60,14 @@ vector<vector<int>> updateEnemies(int setting, vector<vector<int>> world = world
 	}
 	if(setting <= 0){
 		for(auto i : removeList){
-			world.at(i.at(0)).at(i.at(1)) = -1;
+			goldList[{i.at(0), i.at(1)}] = i.at(2);
+			world.at(i.at(0)).at(i.at(1)) = -5;
 		};
 	}
 	return world;
 }
 
-void buildLists(vector<vector<int>> callingList){ //finds the list which contains the stats for the enemies found inthe relational database
+void buildLists(vector<vector<int>> callingList){//finds the list which contains the stats for the enemies found inthe relational database
 	playerStatList = setStats(0);
 	for(auto i : callingList){
 		enemyTypeList.emplace_back(i.at(0));
@@ -71,7 +77,7 @@ void buildLists(vector<vector<int>> callingList){ //finds the list which contain
 	};
 }
 
-vector<vector<int>> createWorld(int width, int height, vector<vector<int>> callingList) { //creates the map for the game
+vector<vector<int>> createWorld(int width, int height, vector<vector<int>> callingList) {//creates the map for the game
 	vector<vector<int>> world;
 	for(int i=0; i < height; i++){
 		vector<int> layer;
@@ -80,15 +86,17 @@ vector<vector<int>> createWorld(int width, int height, vector<vector<int>> calli
 		};
 		world.push_back(layer);
 	};
+	world[0][0] = -3;
 	buildLists(callingList);
 	return updatePlayer(0, 0, updateEnemies(1, world), true);
 }
 
-void printWorld(){; //shows the created map and assigns all of the entities to different characthers.
+void printWorld(){//shows the created map and assigns all of the entities to different characters.
 	map<int, string> characters;
+	characters[-5] = "G";
 	characters[-4] = " ";
 	characters[-3] = "S";
-	characters[-2] = "^";
+	characters[-2] = "&";
 	characters[-1] = ".";
 	string line = " "+string(world.at(0).size()*2-1, ' ')+" \n ";
 	string out;
@@ -99,7 +107,7 @@ void printWorld(){; //shows the created map and assigns all of the entities to d
 		for(auto j : i){
 			string jstring;
 			if(j >= 0){
-				attron(COLOR_PAIR(5));
+				attron(COLOR_PAIR(6));
 				jstring = to_string(enemyTypeList[j])+" ";
 			}
 			else{
@@ -111,20 +119,59 @@ void printWorld(){; //shows the created map and assigns all of the entities to d
 		}
 		addstr("\n");
 	}
-	//return out.c_str();
+	addstr(string(world.at(0).size()*2+1, '-').c_str());
+	addstr("\n");
 }
 
-void enemyDie(int ref, vector<vector<int>> enemyLocList, vector<int> enemyTypeList, vector<map<char, int>> enemyStatList){ //The function which handles the enemy dying. Reponsible for removing enemies from the database and the game
+void printHealth(){
+	int worldWidth = world.at(0).size();
+	float health = playerStatList['H']/100.0;
+	int upto = health * worldWidth * 2;
+	vector<int> uptoList = {worldWidth/2, worldWidth, worldWidth/2};
+	for(int i = 0; i < uptoList.size(); i++){
+		if(upto > uptoList.at(i)){
+			upto -= uptoList.at(i);
+		}
+		else{
+			uptoList.at(i) = upto; 
+
+			upto = 0;
+		}
+	}
+	string healthBar1 = string(uptoList[0],' ');
+	string healthBar2 = string(uptoList[1]+1,' ');
+	string healthBar3 = string(uptoList[2],' ');
+	attron(COLOR_PAIR(10));
+	addstr(healthBar1.c_str());
+	attroff(COLOR_PAIR(10));
+	attron(COLOR_PAIR(9));
+	addstr(healthBar2.c_str());
+	attroff(COLOR_PAIR(9));
+	attron(COLOR_PAIR(8));
+	addstr(healthBar3.c_str());
+	attroff(COLOR_PAIR(8));
+	addstr("\n");
+}
+
+void printGold(){
+	int worldWidth = world.at(0).size();
+	string gold = "Gold : "+to_string(playerStatList['G']);
+	string tab = string(worldWidth-gold.size()/2, ' ');
+	addstr((tab+gold+tab).c_str());
+	addstr("\n");
+}
+
+void enemyDie(int ref, vector<vector<int>> enemyLocList, vector<int> enemyTypeList, vector<map<char, int>> enemyStatList){//The function which handles the enemy dying. Reponsible for removing enemies from the database and the game
 	removeList.emplace_back(enemyLocList.at(ref));
+	removeList.back().emplace_back(enemyStatList.at(ref)['G']);
 	enemyTypeList.erase(enemyTypeList.begin()+ref);
 	enemyStatList.erase(enemyStatList.begin()+ref);
 	enemyLocList.erase(enemyLocList.begin()+ref);
 	world = updateEnemies(0, world);
-	//drop_gold(self.golddropped) // Runs the unction that causes enemies to drop gold
 
 }
 
-void playerAttack(int dx, int dy){ //Handles the player attacking. If used on an enemy depletes their health. If used on a shop, acesses the shop screen
+void playerAttack(int dx, int dy){//Handles the player attacking. If used on an enemy depletes their health. If used on a shop, acesses the shop screen
 	int mapnum = world.at(playerLocation.at(0)+dx).at(playerLocation.at(1)+dy);
 	if(mapnum < 0){
 		return;
@@ -140,7 +187,7 @@ void playerAttack(int dx, int dy){ //Handles the player attacking. If used on an
 	}
 }
 
-bool enemyAttack(){ //run when an enemy attacks the player. Takes points from the player's health depending on how much attack the enemy has and the player's defence
+bool enemyAttack(){//run when an enemy attacks the player. Takes points from the player's health depending on how much attack the enemy has and the player's defence
 	int j;
 	for(int i = 0; i < 4; i+=1){
 		j = i*M_PI/2;
@@ -168,15 +215,14 @@ vector<vector<int>> addObs(int t, int b, int l, int r, vector<vector<int>> world
 	return world;
 }
 
-void gameLoop(){ //Handles the running of the game and also assigns the player functions to different keys
+void gameLoop(){//Handles the running of the game and also assigns the player functions to different keys
 	int key;
-	int keyIndex;
 	vector<int> args;
 	map<int, vector<int>> movingKeys;
-	movingKeys[97] = {0,-1};
-	movingKeys[100] = {0,1};
-	movingKeys[115] = {1,0};
-	movingKeys[119] = {-1,0};
+	movingKeys[97] = {0,-1}; //Left
+	movingKeys[100] = {0,1}; //Right
+	movingKeys[115] = {1,0}; //Down
+	movingKeys[119] = {-1,0}; //Up
 	map<int, vector<int>> attackKeys;
 	attackKeys[49] = {0,-1}; //Left
 	attackKeys[50] = {1,0}; //Down
@@ -190,8 +236,13 @@ void gameLoop(){ //Handles the running of the game and also assigns the player f
 	init_pair(2, COLOR_BLACK, COLOR_YELLOW);
 	init_pair(3, COLOR_BLACK, COLOR_YELLOW);
 	init_pair(4, COLOR_BLACK, COLOR_MAGENTA);
-	init_pair(5, COLOR_RED, COLOR_YELLOW);
-	printWorld();
+	init_pair(5, COLOR_BLACK, COLOR_YELLOW);
+	init_pair(6, COLOR_RED, COLOR_YELLOW);
+	init_pair(7, COLOR_WHITE, COLOR_BLACK);
+	init_pair(8, COLOR_BLACK, COLOR_GREEN);
+	init_pair(9, COLOR_BLACK, COLOR_YELLOW);
+	init_pair(10, COLOR_BLACK, COLOR_RED);
+	addstr("Press any key to begin!");
 	refresh();
 	for(int i = 0; i > -1; i++){
 		key = getch();
@@ -203,12 +254,13 @@ void gameLoop(){ //Handles the running of the game and also assigns the player f
 			args = attackKeys[key];
 			playerAttack(args.at(0),args.at(1));
 		}
-		if(enemyAttack() || key == 96){
+		else if(key == 96){ //!!!Enemies attack disabled!!!
 			break;
 		}
 		clear();
 		printWorld();
-		addstr(to_string(i).c_str());
+		printHealth();
+		printGold();
 		refresh();
 	}
 	clear();
@@ -218,9 +270,14 @@ void gameLoop(){ //Handles the running of the game and also assigns the player f
 	endwin();
 }
 
-int main(){
-	world = createWorld(20, 20, {{4,4,5},{6,3,5}});
-	world = addObs(14,20,15,20,world);
+int main(int argc, char *argv[]){
+	if(argc >= 2 && stoi(argv[1]) > 5 && stoi(argv[2]) > 5){
+		world = createWorld(stoi(argv[1]), stoi(argv[2]), {{6,3,5}});
+	}
+	else{
+		world = createWorld(20, 20, {{6,3,5}});
+	}
+	//world = addObs(14,20,15,20,world);
 	gameLoop();
 
 	return 0;
